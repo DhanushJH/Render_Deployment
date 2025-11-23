edecccb0b547e20bf89aa872d9a75d3d20d3528c
Dhanush J H <dhanushjh1@gmail.com>
2025-11-23 07:16:26 +0530

    feat; /add joke feature is now live

--- Day5.cpp
+++ Day5.cpp
@@ -9,6 +9,25 @@
 
 using json = nlohmann::json;
 
+std::string sGenerateId()
+{
+    auto Time = std::time(nullptr);
+    auto RandomNum = rand() % 100000;
+
+    return std::to_string(Time) + "-" + std::to_string(RandomNum);
+}
+
+std::string sGetTimestamp()
+{
+    auto Time = std::time(nullptr);
+    std::tm* LocalTime = std::localtime(&Time);
+
+    char cTimeBuffer[32];
+    std::strftime(cTimeBuffer, sizeof(cTimeBuffer), "%Y:%m:%d %H:%M:%S", LocalTime);
+
+    return cTimeBuffer;
+}
+
 json loadtojson()
 {
     json DatainFormat = json::array();
@@ -38,6 +57,7 @@ void savetofile(const json& DatainFormat)       // here constant is used as obje
 int main()
 {
     std::srand(static_cast<unsigned>(std::time(nullptr)));
+    srand(time(NULL));
 
     httplib::Server svr;
     // Allow CORS Preflight
@@ -92,6 +112,40 @@ int main()
         int index = std::rand() % static_cast<int>(db.size());
         out = db[index];
         res.set_content(out.dump(4), "application/json");
+    });
+
+    //Add new Joke to the File
+    svr.Post("/add", [](const httplib::Request& req, httplib::Response& res)
+    {
+        if(!req.body.size())
+        {
+            res.status = 400;
+            res.set_content("{\"error\":\"Request Body is missing\"}", "application/json");
+            return;
+        }
+
+        json RequestBody = json::parse(req.body, nullptr, false);
+        if(!RequestBody.contains("joke"))
+        {
+            res.status = 400;
+            res.set_content("{\"error\":\"Joke is missing\"}", "application/json");
+            return;
+        }
+
+        json db = loadtojson();
+        json entry;
+        entry["id"] = sGenerateId();
+        entry["timestamp"] = sGetTimestamp();
+        entry["joke"] = RequestBody["joke"];
+
+        db.push_back(entry);
+        savetofile(db);
+
+        json ResponseBody;
+        ResponseBody["status"] = "Saved";
+        ResponseBody["id"] = entry["id"];
+        ResponseBody["count"] = db.size();
+        res.set_content(ResponseBody.dump(4), "application/json");
     });
 
     //Health - to keep the Render site alive from inactivity
